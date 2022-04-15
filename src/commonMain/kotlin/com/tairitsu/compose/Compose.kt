ae16c7d106991a2561276f8d056cebdd1fc51cf6
Eric_Lian <git@superexercisebook.com>
2022-04-15 14:17:38 +0800

    refactor: use generic instead of polymorphism

--- src/commonMain/kotlin/com/tairitsu/compose/Compose.kt
+++ src/commonMain/kotlin/com/tairitsu/compose/Compose.kt
@@ -93,17 +93,13 @@ fun Difficulties.beyond(closure: Difficulty.() -> Unit) {
 }
 
 // Timing
-
-fun Difficulty.timing(offset: Int, bpm: Double, beats: Double) = timing(offset.toLong(), bpm, beats)
-fun Difficulty.timing(offset: Int, bpm: Int, beats: Double) = timing(offset.toLong(), bpm.toDouble(), beats)
-fun Difficulty.timing(offset: Int, bpm: Double, beats: Int) = timing(offset.toLong(), bpm, beats.toDouble())
-fun Difficulty.timing(offset: Int, bpm: Int, beats: Int) = timing(offset.toLong(), bpm.toDouble(), beats.toDouble())
-fun Difficulty.timing(offset: Long, bpm: Double, beats: Int) = timing(offset, bpm, beats.toDouble())
-fun Difficulty.timing(offset: Long, bpm: Int, beats: Double) = timing(offset, bpm.toDouble(), beats)
-fun Difficulty.timing(offset: Long, bpm: Int, beats: Int) = timing(offset, bpm.toDouble(), beats.toDouble())
-fun Difficulty.timing(offset: Long, bpm: Double, beats: Double): TimingGroup.Timing {
+fun <TOffset : Number, TBpm : Number, TBeat : Number> Difficulty.timing(
+    offset: TOffset,
+    bpm: TBpm,
+    beats: TBeat,
+): TimingGroup.Timing {
     val ctx = this.currentTimingGroup
-    val ret = TimingGroup.Timing(offset, bpm, beats)
+    val ret = TimingGroup.Timing(offset.toLong(), bpm.toDouble(), beats.toDouble())
     ctx.timing.add(ret)
     return ret
 }
@@ -123,22 +119,18 @@ fun Difficulty.timingGroup(name: String = uuid4().toString(), closure: TimingGro
 
 // Normal Note
 
-fun Difficulty.normalNote(time: Int, column: Int) = normalNote(time.toLong(), column)
-fun Difficulty.normalNote(time: Long, column: Int): NormalNote {
+fun <TTime : Number> Difficulty.normalNote(time: TTime, column: Int): NormalNote {
     val ctx = this.currentTimingGroup
-    val ret = NormalNote(time, column)
+    val ret = NormalNote(time.toLong(), column)
     ctx.notes.add(ret)
     return ret
 }
 
 // Hold Note
 
-fun Difficulty.holdNote(time: Int, endTime: Int, column: Int) = holdNote(time.toLong(), endTime.toLong(), column)
-fun Difficulty.holdNote(time: Long, endTime: Int, column: Int) = holdNote(time, endTime.toLong(), column)
-fun Difficulty.holdNote(time: Int, endTime: Long, column: Int) = holdNote(time.toLong(), endTime, column)
-fun Difficulty.holdNote(time: Long, endTime: Long, column: Int): HoldNote {
+fun <TTime : Number, TEndTime : Number> Difficulty.holdNote(time: TTime, endTime: TEndTime, column: Int): HoldNote {
     val ctx = this.currentTimingGroup
-    val ret = HoldNote(time, endTime, column)
+    val ret = HoldNote(time.toLong(), endTime.toLong(), column)
     ctx.notes.add(ret)
     return ret
 }
@@ -162,42 +154,9 @@ val Difficulty.sisi: ArcNote.Type
 val Difficulty.soso: ArcNote.Type
     get() = ArcNote.Type.SOSO
 
-fun Difficulty.arcNote(
-    time: Int,
-    endTime: Int,
-    startPosition: Position,
-    curveType: ArcNote.Type,
-    endPosition: Position,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time.toLong(), endTime.toLong(), startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Long,
-    endTime: Int,
-    startPosition: Position,
-    curveType: ArcNote.Type,
-    endPosition: Position,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time, endTime.toLong(), startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Int,
-    endTime: Long,
-    startPosition: Position,
-    curveType: ArcNote.Type,
-    endPosition: Position,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time.toLong(), endTime, startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Long,
-    endTime: Long,
+fun <TTime : Number, TEndTime : Number> Difficulty.arcNote(
+    time: TTime,
+    endTime: TEndTime,
     startPosition: Position,
     curveType: ArcNote.Type,
     endPosition: Position,
@@ -206,56 +165,37 @@ fun Difficulty.arcNote(
     arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
 ): ArcNote {
     val ctx = this.currentTimingGroup
-    val ret = ArcNote(time, endTime, startPosition, curveType, endPosition, color ?: ArcNote.Color.BLUE, isGuidingLine, arcTapClosure)
+    val ret = ArcNote(time.toLong(),
+        endTime.toLong(),
+        startPosition,
+        curveType,
+        endPosition,
+        color ?: ArcNote.Color.BLUE,
+        isGuidingLine,
+        arcTapClosure)
     ctx.notes.add(ret)
     return ret
 }
 
-fun Difficulty.arcNote(
-    time: Int,
-    endTime: Int,
-    startPosition: Pair<Double, Double>,
+fun <TTime : Number, TEndTime : Number, TStartPositionX : Number, TStartPositionY : Number, TEndPositionX : Number, TEndPositionY : Number> Difficulty.arcNote(
+    time: TTime,
+    endTime: TEndTime,
+    startPosition: Pair<TStartPositionX, TStartPositionY>,
     curveType: ArcNote.Type,
-    endPosition: Pair<Double, Double>,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time.toLong(), endTime.toLong(), startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Long,
-    endTime: Int,
-    startPosition: Pair<Double, Double>,
-    curveType: ArcNote.Type,
-    endPosition: Pair<Double, Double>,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time, endTime.toLong(), startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Int,
-    endTime: Long,
-    startPosition: Pair<Double, Double>,
-    curveType: ArcNote.Type,
-    endPosition: Pair<Double, Double>,
-    color: ArcNote.Color? = null,
-    isGuidingLine: Boolean = color == null,
-    arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
-) = arcNote(time.toLong(), endTime, startPosition, curveType, endPosition, color, isGuidingLine, arcTapClosure)
-
-fun Difficulty.arcNote(
-    time: Long,
-    endTime: Long,
-    startPosition: Pair<Double, Double>,
-    curveType: ArcNote.Type,
-    endPosition: Pair<Double, Double>,
+    endPosition: Pair<TEndPositionX, TEndPositionY>,
     color: ArcNote.Color? = null,
     isGuidingLine: Boolean = color == null,
     arcTapClosure: (ArcNote.ArcTapList.() -> Unit) = {},
 ): ArcNote {
     val ctx = this.currentTimingGroup
-    val ret = ArcNote(time, endTime, startPosition, curveType, endPosition, color ?: ArcNote.Color.BLUE, isGuidingLine, arcTapClosure)
+    val ret = ArcNote(time.toLong(),
+        endTime.toLong(),
+        startPosition.first.toDouble() to startPosition.second.toDouble(),
+        curveType,
+        endPosition.first.toDouble() to endPosition.second.toDouble(),
+        color ?: ArcNote.Color.BLUE,
+        isGuidingLine,
+        arcTapClosure)
     ctx.notes.add(ret)
     return ret
 }
